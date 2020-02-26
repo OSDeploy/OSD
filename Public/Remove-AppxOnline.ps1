bce360d20208a5ed5a8266042038e60fe60d49a8
David Segura <david@segura.org>
2020-02-26 11:43:40 -0600

    19.12.20.0

--- Public/Remove-AppxOnline.ps1
+++ Public/Remove-AppxOnline.ps1
@@ -52,7 +52,7 @@ function Remove-AppxOnline {
             if ($GridRemoveAppx.IsPresent) {
                 Get-AppxPackage | Select-Object * | Where-Object {$_.NonRemovable -ne $true} | Out-GridView -PassThru -Title "Select Appx Packages to Remove from Online Windows Image" | ForEach-Object {
                     Write-Verbose "$($_.Name): Removing Appx Package $($_.PackageFullName)" -Verbose
-                    #Remove-AppPackage -AllUsers -Package $_.PackageFullName -Verbose
+                    Remove-AppPackage -AllUsers -Package $_.PackageFullName -Verbose
                 }
             }
         }
@@ -63,7 +63,7 @@ function Remove-AppxOnline {
             if ($GridRemoveAppxPP.IsPresent) {
                 Get-AppxProvisionedPackage -Online | Select-Object DisplayName, PackageName | Out-GridView -PassThru -Title "Select Appx Provisioned Packages to Remove from Online Windows Image" | ForEach-Object {
                     Write-Verbose "$($_.DisplayName): Removing Appx Provisioned Package $($_.PackageName)" -Verbose
-                    #Remove-AppProvisionedPackage -Online -AllUsers -PackageName $_.PackageName
+                    Remove-AppProvisionedPackage -Online -AllUsers -PackageName $_.PackageName
                 }
             }
         }
@@ -76,24 +76,24 @@ function Remove-AppxOnline {
                     Get-AppxPackage -AllUsers | Select-Object * | Where-Object {$_.NonRemovable -ne $true} | Where-Object {$_.Name -Match $Item} | ForEach-Object {
                         if ((Get-Command Remove-AppxPackage).Parameters.ContainsKey('AllUsers')) {
                             Write-Verbose "$($_.Name): Remove AllUsers Appx Package $($_.PackageFullName)" -Verbose
-                            #Try {Remove-AppxPackage -AllUsers -Package $_.PackageFullName | Out-Null}
-                            #Catch {Write-Warning "$($_.Name): Remove AllUsers Appx Package $($_.PackageFullName) did not complete successfully"}
+                            Try {Remove-AppxPackage -AllUsers -Package $_.PackageFullName | Out-Null}
+                            Catch {Write-Warning "$($_.Name): Remove AllUsers Appx Package $($_.PackageFullName) did not complete successfully"}
                         } else {
                             Write-Verbose "$($_.Name): Remove Appx Package $($_.PackageFullName)" -Verbose
-                            #Try {Remove-AppxPackage -Package $_.PackageFullName | Out-Null}
-                            #Catch {Write-Warning "$($_.Name): Remove Appx Package $($_.PackageFullName) did not complete successfully"}
+                            Try {Remove-AppxPackage -Package $_.PackageFullName | Out-Null}
+                            Catch {Write-Warning "$($_.Name): Remove Appx Package $($_.PackageFullName) did not complete successfully"}
                         }
                     }
                 } else {
                     Get-AppxPackage | Select-Object * | Where-Object {$_.NonRemovable -ne $true} | Where-Object {$_.Name -Match $Item} | ForEach-Object {
                         if ((Get-Command Remove-AppxPackage).Parameters.ContainsKey('AllUsers')) {
                             Write-Verbose "$($_.Name): Remove AllUsers Appx Package $($_.PackageFullName)" -Verbose
-                            #Try {Remove-AppxPackage -AllUsers -Package $_.PackageFullName | Out-Null}
-                            #Catch {Write-Warning "$($_.Name): Remove AllUsers Appx Package $($_.PackageFullName) did not complete successfully"}
+                            Try {Remove-AppxPackage -AllUsers -Package $_.PackageFullName | Out-Null}
+                            Catch {Write-Warning "$($_.Name): Remove AllUsers Appx Package $($_.PackageFullName) did not complete successfully"}
                         } else {
                             Write-Verbose "$($_.Name): Remove Appx Package $($_.PackageFullName)" -Verbose
-                            #Try {Remove-AppxPackage -Package $_.PackageFullName | Out-Null}
-                            #Catch {Write-Warning "$($_.Name): Remove Appx Package $($_.PackageFullName) did not complete successfully"}
+                            Try {Remove-AppxPackage -Package $_.PackageFullName | Out-Null}
+                            Catch {Write-Warning "$($_.Name): Remove Appx Package $($_.PackageFullName) did not complete successfully"}
                         }
                     }
                 }
@@ -102,12 +102,12 @@ function Remove-AppxOnline {
                 Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -Match $Item} | ForEach-Object {
                     if ((Get-Command Remove-AppxProvisionedPackage).Parameters.ContainsKey('AllUsers')) {
                         Write-Verbose "$($_.DisplayName): Remove AllUsers Appx Provisioned Package $($_.PackageName)" -Verbose
-                        #Try {Remove-AppxProvisionedPackage -Online -AllUsers -PackageName $_.PackageName | Out-Null}
-                        #Catch {Write-Warning "$($_.DisplayName): Remove AllUsers Appx Provisioned Package $($_.PackageName) did not complete successfully"}
+                        Try {Remove-AppxProvisionedPackage -Online -AllUsers -PackageName $_.PackageName | Out-Null}
+                        Catch {Write-Warning "$($_.DisplayName): Remove AllUsers Appx Provisioned Package $($_.PackageName) did not complete successfully"}
                     } else {
                         Write-Verbose "$($_.DisplayName): Remove Appx Provisioned Package $($_.PackageName)" -Verbose
-                        #Try {Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName | Out-Null}
-                        #Catch {Write-Warning "$($_.DisplayName): Remove Appx Provisioned Package $($_.PackageName) did not complete successfully"}
+                        Try {Remove-AppxProvisionedPackage -Online -PackageName $_.PackageName | Out-Null}
+                        Catch {Write-Warning "$($_.DisplayName): Remove Appx Provisioned Package $($_.PackageName) did not complete successfully"}
                     }
                 }
             }
