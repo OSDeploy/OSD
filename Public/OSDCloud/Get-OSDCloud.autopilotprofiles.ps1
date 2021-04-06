541750c41f74bc5f2311d993836c177464d3604b
David Segura <david@segura.org>
2021-04-06 18:42:07 -0500

    Unique results

--- Public/OSDCloud/Get-OSDCloud.autopilotprofiles.ps1
+++ Public/OSDCloud/Get-OSDCloud.autopilotprofiles.ps1
@@ -1,11 +1,33 @@
+<#
+.SYNOPSIS
+Searches all PSDrives for AutoPilot Profiles [PSDrive]:\OSDCloud\AutoPilot\Profiles
+
+.Description
+Searches all PSDrives for AutoPilot Profiles [PSDrive]:\OSDCloud\AutoPilot\Profiles
+
+.LINK
+https://osdcloud.osdeploy.com
+
+.NOTES
+21.4.6  Modified function to display Unique results
+21.4.6  Removed Name parameter as that served no purpose
+#>
 function Get-OSDCloud.autopilotprofiles {
     [CmdletBinding()]
-    param (
-        [string]$Name
-    )
-    $OSDCloudAutoOfflinePilotProfiles = @()
-    $OSDCloudAutoOfflinePilotProfiles = Get-PSDrive -PSProvider FileSystem | ForEach-Object {
+    param ()
+    #=======================================================================
+    #	Create the Array
+    #=======================================================================
+    $Results = @()
+    #=======================================================================
+    #	Search for AutoPilot Profiles
+    #=======================================================================
+    $Results = Get-PSDrive -PSProvider FileSystem | ForEach-Object {
         Get-ChildItem "$($_.Name):\OSDCloud\AutoPilot\Profiles" -Include *.json -File -Recurse -Force -ErrorAction Ignore
     }
-    $OSDCloudAutoOfflinePilotProfiles
+    #=======================================================================
+    #	Results
+    #=======================================================================
+    $Results | Sort-Object -Property Name -Unique
+    #=======================================================================
 }
