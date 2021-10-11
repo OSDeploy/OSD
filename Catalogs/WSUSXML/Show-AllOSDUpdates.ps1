cbad1bd7c4da0ebee904d9a8cc62e7cc13a82ae2
David Segura <david@segura.org>
2021-10-11 11:57:13 -0500

    21.10.11.1

--- Catalogs/WSUSXML/Show-AllOSDUpdates.ps1
+++ Catalogs/WSUSXML/Show-AllOSDUpdates.ps1
@@ -4,4 +4,4 @@ foreach ($UpdateCatalog in $AllUpdateCatalogs) {$AllOSDUpdates += Import-Clixml 
 
 $AllOSDUpdates = $AllOSDUpdates | Select-Object -Property * | Sort-Object -Property Title -Unique | Sort-Object CreationDate -Descending #| Out-GridView -PassThru -Title "All OSDUpdates"
 Write-Host ""
-$AllOSDUpdates | Select-Object -Property CreationDate, KBNumber, Title | Sort @{Expression = {$_.CreationDate}; Ascending = $false}, KBNumber, Title | Out-File D:\GitHub\Modules\WSUSXML\UPDATES.md
+$AllOSDUpdates | Select-Object -Property CreationDate, KBNumber, Title | Sort @{Expression = {$_.CreationDate}; Ascending = $false}, KBNumber, Title | Out-File D:\GitHub\Modules\OSD\UPDATES.md
