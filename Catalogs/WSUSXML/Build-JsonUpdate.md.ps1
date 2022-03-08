c163a93243ccb22de06dc6b257565847dd0e246d
David Segura <david@segura.org>
2022-03-08 13:26:16 -0600

    22.3.8.3

--- Catalogs/WSUSXML/Build-JsonUpdate.md.ps1
+++ Catalogs/WSUSXML/Build-JsonUpdate.md.ps1
@@ -1,3 +1,4 @@
+Import-Module OSD -Force
 $AllUpdates = @()
 $AllCatalogs = Get-ChildItem -Path "$PSScriptRoot\*" -Include '*.json' -Recurse
 foreach ($CatalogFile in $AllCatalogs) {
@@ -6,4 +7,4 @@ foreach ($CatalogFile in $AllCatalogs) {
 
 $AllUpdates = $AllUpdates | Select-Object -Property * | Sort-Object -Property Title -Unique | Sort-Object CreationDate -Descending
 Write-Host ""
-$AllUpdates | Select-Object -Property CreationDate, KBNumber, Title | Sort @{Expression = {$_.CreationDate}; Ascending = $false}, KBNumber, Title | Out-File D:\GitHub\Modules\OSD\UPDATES.md
+$AllUpdates | Select-Object -Property CreationDate, KBNumber, Title | Sort-Object @{Expression = {$_.CreationDate}; Ascending = $false}, KBNumber, Title | Out-File D:\GitHub\MyModules\OSD\UPDATES.md
