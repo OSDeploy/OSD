f35d842b23cb31ded2a984c956ed345971406f62
David Segura <david@segura.org>
2022-03-28 22:27:09 -0500

    Update hash.ps1

--- cloudscript/hash.ps1
+++ cloudscript/hash.ps1
@@ -15,7 +15,7 @@
 Script should be executed in a Command Prompt using the following command
 powershell Invoke-Expression -Command (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/OSDeploy/OSD/master/cloudscript/hash.ps1')
 This is abbreviated as
-powershell iex (irm raw.githubusercontent.com/OSDeploy/OSD/master/cloudscript/hash.ps1)
+powershell iex (irm go.osdcloud.com/hash)
 #>
 <#
 .SYNOPSIS
@@ -33,23 +33,29 @@ if ($env:SystemDrive -eq 'X:') {
     Write-Warning "This script cannot be run from WinPE"
 }
 else {
-    $bad = $false
-    $session = New-CimSession
-    $serial = (Get-CimInstance -CimSession $session -Class Win32_BIOS).SerialNumber
-    $devDetail = (Get-CimInstance -CimSession $session -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'")
-    if ($devDetail) {
-        $hash = $devDetail.DeviceHardwareData
+    $IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+    if (! $IsAdmin) {
+        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) go.osdcloud.com/hash requires elevated Admin Rights"
     }
     else {
-        $bad = $true
-        $hash = ""
+        $bad = $false
+        $session = New-CimSession
+        $serial = (Get-CimInstance -CimSession $session -Class Win32_BIOS).SerialNumber
+        $devDetail = (Get-CimInstance -CimSession $session -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'")
+        if ($devDetail) {
+            $hash = $devDetail.DeviceHardwareData
+        }
+        else {
+            $bad = $true
+            $hash = ""
+        }
+    
+        if ($bad) {
+            Write-Error -Message "Unable to retrieve device hardware data (hash) from computer" -Category DeviceError
+        }
+        else {
+            $hash
+        }
+        Remove-CimSession $session
     }
-
-    if ($bad) {
-        Write-Error -Message "Unable to retrieve device hardware data (hash) from computer $comp" -Category DeviceError
-    }
-    else {
-        $hash
-    }
-    Remove-CimSession $session
 }
