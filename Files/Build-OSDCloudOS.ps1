62f47043bb5f11b92adfd4efd865e265ee2a3d5d
David Segura <david@segura.org>
2022-04-05 23:16:41 -0500

    22.4.5.1

--- Files/Build-OSDCloudOS.ps1
+++ Files/Build-OSDCloudOS.ps1
@@ -4,44 +4,82 @@ Import-Module -Name OSD -Force
 #=================================================
 $Results = Get-WSUSXML -Catalog FeatureUpdate -Silent
 $Results = $Results | Where-Object {$_.UpdateArch -eq 'x64'}
-$Results = $Results | Select-Object @{Name='CatalogVersion';Expression={(Get-Date -Format "yy.MM.dd")}}, `
+$Results = $Results | Select-Object `
+@{Name='Catalog';Expression={('OS')}}, `
+@{Name='Updated';Expression={(Get-Date -Format "yyyy-MM-dd")}}, `
 @{Name='Status';Expression={($_.OSDStatus)}}, `
-@{Name='ReleaseDate';Expression={(Get-Date $_.CreationDate -Format "yy.MM.dd")}}, `
-@{Name='Title';Expression={($_.Title)}}, `
-@{Name='OSName';Expression={($null)}}, `
-@{Name='OSVersion';Expression={($_.UpdateOS)}}, `
-@{Name='OSArchitecture';Expression={($_.UpdateArch)}}, `
-@{Name='OSBuild';Expression={($_.UpdateBuild)}}, `
-@{Name='OSLanguage';Expression={($null)}}, `
-@{Name='OSLicense';Expression={($null)}}, `
+@{Name='Released';Expression={(Get-Date $_.CreationDate -Format "yyyy-MM-dd")}}, `
+@{Name='Name';Expression={($_.Title)}}, `
+@{Name='OS';Expression={($null)}}, `
+@{Name='Version';Expression={($_.UpdateBuild)}}, `
+@{Name='Build';Expression={($null)}}, `
+@{Name='Arch';Expression={($_.UpdateArch)}}, `
+@{Name='Language';Expression={($null)}}, `
+@{Name='Activation';Expression={($null)}}, `
 @{Name='FileName';Expression={((Split-Path -Leaf $_.FileUri))}}, `
+@{Name='ImageIndex';Expression={($null)}}, `
+@{Name='ImageName';Expression={($null)}}, `
 @{Name='Url';Expression={($_.FileUri)}}, `
-@{Name='HashSHA1';Expression={($null)}}
+@{Name='SHA1';Expression={($null)}}, `
+@{Name='UpdateID';Expression={($_.UpdateID)}}, `
+@{Name='Win10';Expression={($null)}}, `
+@{Name='Win11';Expression={($null)}}
 
 foreach ($Result in $Results) {
-    $Result.OSName = $Result.OSVersion + ' ' + $Result.OSBuild
-
+    #=================================================
+    #   Language
+    #=================================================
     if ($Result.FileName -match 'sr-latn-rs') {
-        $Result.OSLanguage = 'sr-latn-rs'
+        $Result.Language = 'sr-latn-rs'
     }
     else {
         $Regex = "[a-zA-Z]+-[a-zA-Z]+"
-        $Result.OSLanguage = ($Result.FileName | Select-String -AllMatches -Pattern $Regex).Matches[0].Value
+        $Result.Language = ($Result.FileName | Select-String -AllMatches -Pattern $Regex).Matches[0].Value
     }
-
-    if ($Result.Title -match 'business') {
-        $Result.OSLicense = 'Volume'
+    #=================================================
+    #   Activation
+    #=================================================
+    if ($Result.Url -match 'business') {
+        $Result.Activation = 'Volume'
     }
     else {
-        $Result.OSLicense = 'Retail'
+        $Result.Activation = 'Retail'
     }
-
+    #=================================================
+    #   OS
+    #=================================================
+    if ($Result.Name -match 'Windows 10') {
+        $Result.OS = 'Windows 10'
+        $Result.Win10 = $true
+        $Result.Win11 = $false
+    }
+    if ($Result.Name -match 'Windows 11') {
+        $Result.OS = 'Windows 11'
+        $Result.Win10 = $false
+        $Result.Win11 = $true
+    }
+    #=================================================
+    #   Build
+    #=================================================
+    $Regex = "[0-9]*\.[0-9]+"
+    $Result.Build = ($Result.FileName | Select-String -AllMatches -Pattern $Regex).Matches[0].Value
+    #=================================================
+    #   SHA1
+    #=================================================
     $Regex = "[0-9a-f]{40}"
-    $Result.HashSHA1 = ($Result.FileName | Select-String -AllMatches -Pattern $Regex).Matches[0].Value
-
-    $Result.Title = $Result.OSName + ' ' + $Result.OSLicense + ' ' + $Result.OSLanguage
+    $Result.SHA1 = ($Result.FileName | Select-String -AllMatches -Pattern $Regex).Matches[0].Value
+    #=================================================
+    #   Name
+    #=================================================
+    if ($Result.Activation -eq 'Volume') {
+        $Result.Name = $Result.OS + ' ' + $Result.Version + ' x64 ' + $Result.Language + ' business editions'
+    }
+    else {
+        $Result.Name = $Result.OS + ' ' + $Result.Version + ' x64 ' + $Result.Language + ' consumer editions'
+    }
+    #=================================================
 }
-
+$Results = $Results | Sort-Object -Property Name
 $Results | Export-Clixml -Path (Join-Path (Get-Module OSD).ModuleBase "OSDCloud\os.xml") -Force
 Import-Clixml -Path (Join-Path (Get-Module OSD).ModuleBase "OSDCloud\os.xml") | ConvertTo-Json | Out-File (Join-Path (Get-Module OSD).ModuleBase "OSDCloud\os.json") -Force -Encoding ascii
 #================================================
