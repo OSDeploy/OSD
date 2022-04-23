f8d08a62ed96f93163c4612033d187c598af39f4
David Segura <david@segura.org>
2022-04-23 12:00:11 -0500

    22.4.23.1

--- cloudscript/Get-AzAccessToken.ps1
+++
@@ -1,8 +0,0 @@
-$global:accessToken = (Get-AzAccessToken -ResourceUrl https://graph.microsoft.com).Token
-$global:authHeader = @{
-    'Content-Type' = 'application/json'
-    'Authorization' = 'Bearer ' + $global:accessToken
-}
-$global:accessToken
-Write-Verbose -Verbose '$global:accessToken contains the Azure Access Token'
-Write-Verbose -Verbose '$global:authHeader contains the HTTP Header Authorization'
