e6bc086ec339ef13950042d1e119529cbec5dff2
David Segura <david@segura.org>
2022-04-23 04:34:35 -0500

    Update winpegraph.ps1

--- cloudscript/winpegraph.ps1
+++ cloudscript/winpegraph.ps1
@@ -3,19 +3,25 @@ Install-Module WindowsAutopilotIntune -Force
 #Install-Module Microsoft.Graph.Intune -Force
 
 Write-Verbose -Verbose 'Connect-MgGraph -Device -Scopes "DeviceManagementConfiguration.Read.All","DeviceManagementServiceConfig.Read.All"'
-Connect-MgGraph -Device -Scopes "DeviceManagementConfiguration.Read.All","DeviceManagementServiceConfig.Read.All"
+Connect-MgGraph -Device -Scopes DeviceManagementConfiguration.Read.All,DeviceManagementServiceConfig.Read.All,DeviceManagementServiceConfiguration.Read.All
 
 Write-Verbose -Verbose 'Select-MgProfile -Name beta'
 Select-MgProfile -Name beta
 
-Write-Verbose -Verbose 'Get-MgContext'
-Get-MgContext
+Write-Verbose -Verbose '$Global:MgContext'
+$Global:MgContext = Get-MgContext
+$Global:MgContext
 
-Write-Verbose -Verbose '(Get-MgContext).Scopes'
-(Get-MgContext).Scopes
+Write-Verbose -Verbose '$Global:ClientId'
+$Global:MgClientId = $Global:MgContext.ClientId
+$Global:MgClientId
 
-Write-Verbose -Verbose 'Get-AutopilotProfile | ConvertTo-AutopilotConfigurationJSON'
-Get-AutopilotProfile | ConvertTo-AutopilotConfigurationJSON
+Write-Verbose -Verbose '$Global:TenantId'
+$Global:MgTenantId = $Global:MgContext.TenantId
+$Global:MgTenantId
 
-Write-Verbose -Verbose 'Get-MgDeviceManagementScript | Format-List'
-Get-MgDeviceManagementScript | Format-List
+Write-Verbose -Verbose '$Global:Scopes'
+$Global:MgScopes = $Global:MgContext.Scopes
+$Global:MgScopes
+
+Connect-AzAccount -TenantId $Global:MgTenantId -ClientId $Global:MgClientId -AuthScope Storage
