96f64c835e57e29c70db1fe30465a8d69a4d257e
David Segura <david@segura.org>
2022-04-23 02:27:02 -0500

    Update winpegraph.ps1

--- cloudscript/winpegraph.ps1
+++ cloudscript/winpegraph.ps1
@@ -1,8 +1,9 @@
 Install-Module Microsoft.Graph.DeviceManagement -Force
-Install-Module Microsoft.Graph.Intune -Force
+Install-Module WindowsAutopilotIntune -Force
+#Install-Module Microsoft.Graph.Intune -Force
 
-Write-Verbose -Verbose 'Connect-MgGraph -Device -Scopes DeviceManagementConfiguration.ReadWrite.All'
-Connect-MgGraph -Device -Scopes DeviceManagementConfiguration.ReadWrite.All
+Write-Verbose -Verbose 'Connect-MgGraph -Device -Scopes "DeviceManagementConfiguration.Read.All","DeviceManagementServiceConfig.Read.All"'
+Connect-MgGraph -Device -Scopes "DeviceManagementConfiguration.Read.All","DeviceManagementServiceConfig.Read.All"
 
 Write-Verbose -Verbose 'Select-MgProfile -Name beta'
 Select-MgProfile -Name beta
@@ -12,6 +13,9 @@ Get-MgContext
 
 Write-Verbose -Verbose '(Get-MgContext).Scopes'
 (Get-MgContext).Scopes
+
+Write-Verbose -Verbose 'Get-AutopilotProfile | ConvertTo-AutopilotConfigurationJSON'
+Get-AutopilotProfile | ConvertTo-AutopilotConfigurationJSON
 
 Write-Verbose -Verbose 'Get-MgDeviceManagementScript | Format-List'
 Get-MgDeviceManagementScript | Format-List
