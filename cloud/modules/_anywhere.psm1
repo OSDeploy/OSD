52bbde171ad84bafca60aff70d444dad6ea496ea
gwblok <garywblok@gmail.com>
2022-05-20 22:13:25 -0500

    Update _anywhere.psm1
    Added Function osdcloud-EjectCD

--- cloud/modules/_anywhere.psm1
+++ cloud/modules/_anywhere.psm1
@@ -47,6 +47,11 @@ function osdcloud-InstallModuleAutopilot {
         Install-Module WindowsAutopilotIntune -Force -Scope CurrentUser
     }
 }
+function osdcloud-EjectCD {
+    [CmdletBinding()]
+    param ()   
+    (New-Object -ComObject 'Shell.Application').Namespace(17).Items() | Where-Object { $_.Type -eq 'CD Drive' } | ForEach-Object { $_.InvokeVerb('Eject') }
+} 
 function osdcloud-InstallModuleAzAccounts {
     [CmdletBinding()]
     param ()
