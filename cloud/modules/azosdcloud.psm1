9b6d1174e048e57ea5304ebeed10ef1eecf246f9
David Segura <david@segura.org>
2022-05-21 00:32:36 -0500

    22.5.21.1

--- cloud/modules/azosdcloud.psm1
+++ cloud/modules/azosdcloud.psm1
@@ -46,7 +46,7 @@ function Get-AzOSDCloudBlobImage {
                 if ($StorageContainers) {
                     foreach ($Container in $StorageContainers) {
                         Write-Host -ForegroundColor DarkGray "Storage Account: $($Item.StorageAccountName) Container: $($Container.Name)"
-                        $Global:AzOSDCloudBlobImage += Get-AzStorageBlob -Context $Global:AzCurrentStorageContext -Container $Container.Name -Blob *.wim -ErrorAction Ignore
+                        $Global:AzOSDCloudBlobImage += Get-AzStorageBlob -Context $Global:AzCurrentStorageContext -Container $Container.Name -Blob *.wim,*.iso -ErrorAction Ignore
                     }
                 }
             }
