737f514788a1e1f045f5af3a4c45ff65f4c970ad
gwblok <garywblok@gmail.com>
2022-05-26 12:06:05 -0500

    Update functions.ps1
    Updated Version Number

--- cloud/functions.ps1
+++ cloud/functions.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 22.5.23.1
+.VERSION 22.5.26.1
 .GUID 7a3671f6-485b-443e-8e86-b60fdcea1419
 .AUTHOR David Segura @SeguraOSD
 .COMPANYNAME osdcloud.com
@@ -23,7 +23,7 @@ powershell iex (irm functions.osdcloud.com)
 .DESCRIPTION
     PSCloudScript at functions.osdcloud.com
 .NOTES
-    Version 22.5.25.1
+    Version 22.5.26.1
 .LINK
     https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/functions.ps1
 .EXAMPLE
@@ -33,7 +33,7 @@ powershell iex (irm functions.osdcloud.com)
 #=================================================
 #Script Information
 $ScriptName = 'functions.osdcloud.com'
-$ScriptVersion = '22.5.23.1'
+$ScriptVersion = '22.5.26.1'
 #=================================================
 #region Initialize Functions
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
