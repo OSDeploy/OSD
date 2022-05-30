60c325a8b7e43f66593d685b4104365e803ab12d
gwblok <garywblok@gmail.com>
2022-05-29 23:04:30 -0500

    Update functions.ps1
    Beginning to add Dell Support

--- cloud/functions.ps1
+++ cloud/functions.ps1
@@ -52,6 +52,23 @@ function Test-HPIASupport {
     return $HPIASupport
     }
 
+function Test-DCUSupport {
+    $SystemSKUNumber = (Get-CimInstance -ClassName Win32_ComputerSystem).SystemSKUNumber
+    $CabPathIndex = "$env:temp\DellCabDownloads\CatalogIndexPC.cab"
+    $DellCabExtractPath = "$env:temp\DellCabDownloads\DellCabExtract"
+    # Pull down Dell XML CAB used in Dell Command Update ,extract and Load
+    if (!(Test-Path $DellCabExtractPath)){$newfolder = New-Item -Path $DellCabExtractPath -ItemType Directory -Force}
+    Invoke-WebRequest -Uri "https://downloads.dell.com/catalog/CatalogIndexPC.cab" -OutFile $CabPathIndex -UseBasicParsing -ErrorAction SilentlyContinue
+    New-Item -Path $DellCabExtractPath -ItemType Directory -Force | Out-Null
+    $Expand = expand $CabPathIndex $DellCabExtractPath\CatalogIndexPC.xml
+    [xml]$XMLIndex = Get-Content "$DellCabExtractPath\CatalogIndexPC.xml" -ErrorAction SilentlyContinue
+    #Dig Through Dell XML to find Model of THIS Computer (Based on System SKU)
+    $XMLModel = $XMLIndex.ManifestIndex.GroupManifest | Where-Object {$_.SupportedSystems.Brand.Model.systemID -match $SystemSKUNumber}
+    if ($XMLModel){$DCUSupportedDevice = $true}
+    else {$DCUSupportedDevice = $false}
+    Return $DCUSupportedDevice
+    }
+    
 #Determine the proper Windows environment
 if ($env:SystemDrive -eq 'X:') {
     $WindowsPhase = 'WinPE'
