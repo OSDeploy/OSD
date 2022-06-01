d107e24cb53c67f03d91234237331241747b312a
David Segura <david@segura.org>
2022-05-31 23:58:15 -0500

    22.6.1.1

--- cloud/modules/_winpestartup.psm1
+++ cloud/modules/_winpestartup.psm1
@@ -4,7 +4,7 @@
 .DESCRIPTION
     OSDCloud Cloud Module for functions.osdcloud.com
 .NOTES
-    Version 22.5.31.1
+    Version 22.6.1.1
 .LINK
     https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/modules/_winpestartup.psm1
 .EXAMPLE
