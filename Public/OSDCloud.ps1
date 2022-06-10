40525b80f3795e4e7f120f6b6af76433b9d22193
gwblok <gary@thebloks.com>
2022-06-10 14:51:24 -0500

    Updating HP Functions

--- Public/OSDCloud.ps1
+++ Public/OSDCloud.ps1
@@ -1241,9 +1241,10 @@ function Invoke-OSDCloud {
             Add-Content -Path $PSFilePath "Invoke-Expression (Invoke-RestMethod -Uri 'functions.osdcloud.com' -ErrorAction SilentlyContinue)"
             Add-Content -Path $PSFilePath "osdcloud-InstallModuleHPCMSL -ErrorAction SilentlyContinue"
             Add-Content -Path $PSFilePath 'Write-Host "Running HP Tools in SetupComplete" -ForegroundColor Green'
-            if (($Global:OSDCloud.HPIADrivers -eq $true) -and ($Global:OSDCloud.HPIAAll -ne $true)){
+            if ($Global:OSDCloud.HPIADrivers -eq $true){
                 Add-Content -Path $PSFilePath 'Write-Host "Running HPIA for Drivers" -ForegroundColor Magenta'
-                Add-Content -Path $PSFilePath "osdcloud-RunHPIA -Category Drivers"
+                if (Test-Path -path "C:\OSDCloud\HPIA\Repo"){Add-Content -Path $PSFilePath "osdcloud-RunHPIA -OfflineMode True -Category Drivers"}
+                else {Add-Content -Path $PSFilePath "osdcloud-RunHPIA -Category Drivers"}
             }
             if (($Global:OSDCloud.HPIAFirmware -eq $true) -and ($Global:OSDCloud.HPIAAll  -ne $true)){
                 Add-Content -Path $PSFilePath 'Write-Host "Running HPIA for Firmware" -ForegroundColor Magenta'
