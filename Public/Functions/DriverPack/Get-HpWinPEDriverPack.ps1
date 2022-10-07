9538a5425ae7db1c18a117b7019b6b29c7a237b8
David Segura <david@segura.org>
2022-10-07 10:39:20 -0500

    22.10.7
    Resolved issue with HP URL

--- Public/Functions/DriverPack/Get-HpWinPEDriverPack.ps1
+++ Public/Functions/DriverPack/Get-HpWinPEDriverPack.ps1
@@ -13,13 +13,13 @@ function Get-HpWinPEDriverPack {
     [CmdletBinding()]
     param ()
 
-    $LastKnownGood = 'https://ftp.hp.com/pub/softpaq/sp112501-113000/sp112810.exe'
-    $DriverPackInfoUrl = 'http://ftp.ext.hp.com//pub/caps-softpaq/cmit/softpaq/WinPE10.html'
+    $LastKnownGood = 'https://ftp.ext.hp.com/pub/softpaq/sp142501-143000/sp142621.exe'
+    $DriverPackInfoUrl = 'http://ftp.ext.hp.com/pub/caps-softpaq/cmit/softpaq/WinPE10.html'
 
     Write-Verbose $DriverPackInfoUrl
 
     Try {
-        (Invoke-WebRequest -Uri $DriverPackInfoUrl -UseBasicParsing -Method Get).Links | Where-Object {$_.href -like '*ftp.hp.com/pub/softpaq/*/*.exe'} | Select-Object -ExpandProperty href
+        (Invoke-WebRequest -Uri $DriverPackInfoUrl -UseBasicParsing -Method Get).Links | Where-Object {$_.href -like '*ftp.ext.hp.com/pub/softpaq/*/*.exe'} | Select-Object -ExpandProperty href
     }
     Catch {
         Write-Output $LastKnownGood
