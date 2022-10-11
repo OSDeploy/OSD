2809b3a55f2a6d96c73d290c36f2f487e0a92851
David Segura <david@segura.org>
2022-10-11 13:36:01 -0500

    22.10.11.1

--- Catalogs/WSUSXML/Build-JsonUpdate.md.ps1
+++ Catalogs/WSUSXML/Build-JsonUpdate.md.ps1
@@ -7,4 +7,4 @@ foreach ($CatalogFile in $AllCatalogs) {
 
 $AllUpdates = $AllUpdates | Select-Object -Property * | Sort-Object -Property Title -Unique | Sort-Object CreationDate -Descending
 Write-Host ""
-$AllUpdates | Select-Object -Property CreationDate, KBNumber, Title | Sort-Object @{Expression = {$_.CreationDate}; Ascending = $false}, KBNumber, Title | Out-File C:\Users\osdcloud\Documents\GitHub\OSD\UPDATES.md
+$AllUpdates | Select-Object -Property CreationDate, KBNumber, Title | Sort-Object @{Expression = {$_.CreationDate}; Ascending = $false}, KBNumber, Title | Out-File C:\Users\david\OneDrive\GitHub\MyModules\OSD\UPDATES.md
