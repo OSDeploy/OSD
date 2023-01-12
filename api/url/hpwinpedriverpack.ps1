7b5497456076f914dab9d9c3f55ddd94d260e61d
David Segura <david@segura.org>
2023-01-12 08:55:48 -0600

    22.1.12.1

---
+++ api/url/hpwinpedriverpack.ps1
@@ -0,0 +1,11 @@
+$LastKnownGood = 'https://ftp.ext.hp.com/pub/softpaq/sp142501-143000/sp142621.exe'
+$DriverPackInfoUrl = 'http://ftp.ext.hp.com/pub/caps-softpaq/cmit/softpaq/WinPE10.html'
+
+Write-Verbose $DriverPackInfoUrl
+
+Try {
+    (Invoke-WebRequest -Uri $DriverPackInfoUrl -UseBasicParsing -Method Get).Links | Where-Object {$_.href -like '*ftp.ext.hp.com/pub/softpaq/*/*.exe'} | Select-Object -ExpandProperty href
+}
+Catch {
+    Write-Output $LastKnownGood
+}
