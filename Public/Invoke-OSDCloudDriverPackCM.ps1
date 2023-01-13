eebea107ee57be4922ccf317f331439f4d603a5c
gwblok <garywblok@gmail.com>
2023-01-13 10:42:33 -0600

    Update Invoke-OSDCloudDriverPackCM.ps1
    Added support for extracting NEW hp driver packs and applying right in WinPE, added additional task sequence progress bars to show a bit more detail

--- Public/Invoke-OSDCloudDriverPackCM.ps1
+++ Public/Invoke-OSDCloudDriverPackCM.ps1
@@ -8,7 +8,12 @@ function Invoke-OSDCloudDriverPackCM {
 
     .LINK
     https://github.com/OSDeploy/OSD/tree/master/Docs
+
+
+    Requires CURL - typically I run this command first in a previous step,  "iex (irm sandbox.osdcloud.com)", which will setup your WinPE environment along with installing curl.
     #>
+
+    
     [CmdletBinding()]
     param (
         [string]$Manufacturer = (Get-MyComputerManufacturer -Brief),
@@ -29,6 +34,44 @@ function Invoke-OSDCloudDriverPackCM {
         Start-Sleep -Seconds 5
         Continue
     }
+    else {
+        function Confirm-TSProgressUISetup(){
+        if ($Script:TaskSequenceProgressUi -eq $null){
+            try{$Script:TaskSequenceProgressUi = New-Object -ComObject Microsoft.SMS.TSProgressUI}
+            catch{throw "Unable to connect to the Task Sequence Progress UI! Please verify you are in a running Task Sequence Environment. Please note: TSProgressUI cannot be loaded during a prestart command.`n`nErrorDetails:`n$_"}
+            }
+        }
+        function Confirm-TSEnvironmentSetup(){
+            if ($Script:TaskSequenceEnvironment -eq $null){
+                try{$Script:TaskSequenceEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment}
+                catch{throw "Unable to connect to the Task Sequence Environment! Please verify you are in a running Task Sequence Environment.`n`nErrorDetails:`n$_"}
+                }
+            }
+        function Show-TSActionProgress(){
+            param(
+                [Parameter(Mandatory=$true)]
+                [string] $Message,
+                [Parameter(Mandatory=$true)]
+                [long] $Step,
+                [Parameter(Mandatory=$true)]
+                [long] $MaxStep
+            )
+
+            Confirm-TSProgressUISetup
+            Confirm-TSEnvironmentSetup
+            $Script:TaskSequenceProgressUi.ShowActionProgress(`
+                $Script:TaskSequenceEnvironment.Value("_SMSTSOrgName"),`
+                $Script:TaskSequenceEnvironment.Value("_SMSTSPackageName"),`
+                $Script:TaskSequenceEnvironment.Value("_SMSTSCustomProgressDialogMessage"),`
+                $Script:TaskSequenceEnvironment.Value("_SMSTSCurrentActionName"),`
+                [Convert]::ToUInt32($Script:TaskSequenceEnvironment.Value("_SMSTSNextInstructionPointer")),`
+                [Convert]::ToUInt32($Script:TaskSequenceEnvironment.Value("_SMSTSInstructionTableSize")),`
+                $Message,`
+                $Step,`
+                $MaxStep)
+        }
+    }
+
     #=================================================
     #	Get some Task Sequence variables
     #=================================================
@@ -53,7 +96,7 @@ function Invoke-OSDCloudDriverPackCM {
         Continue
     }
 
-    $Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Invoke-OSDCloudDriverPackMDT.log"
+    $Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Invoke-OSDCloudDriverPackCM.log"
     Start-Transcript -Path (Join-Path $OSDiskWindowsDebug $Transcript) -ErrorAction Ignore
     #=================================================
     #	OSDisk Drivers
@@ -91,6 +134,7 @@ function Invoke-OSDCloudDriverPackCM {
         Write-Host -ForegroundColor DarkGray "-Url $($GetMyDriverPack.Url)"
         Write-Host
         $OSDiskDriversFile = Join-Path $OSDiskDrivers $GetMyDriverPack.FileName
+        Show-TSActionProgress -Message "Found Driver Pack: $($GetMyDriverPack.FileName)" -Step 1 -MaxStep 5 -ErrorAction SilentlyContinue
     }
     else {
         Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) There are no DriverPacks for this computer"
@@ -117,21 +161,45 @@ function Invoke-OSDCloudDriverPackCM {
                 Start-Sleep -Seconds 5
                 Continue
             }
+            Show-TSActionProgress -Message "Downloading Driver Pack: $($GetMyDriverPack.Url)" -Step 2 -MaxStep 5 -ErrorAction SilentlyContinue
             Save-WebFile -SourceUrl $GetMyDriverPack.Url -DestinationDirectory $OSDiskDrivers -DestinationName $GetMyDriverPack.FileName
         }
 
         $ReadyDriverPack = Get-ChildItem -Path $OSDiskDrivers -File -Force -ErrorAction Ignore | Where-Object {$_.Name -match $GetMyDriverPackBaseName} | Where-Object {$_.Extension -in ('.cab','.zip','.exe')}
         if ($ReadyDriverPack) {
+            $PPKGMethod = $true
             Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) DriverPack has been copied to $OSDiskDrivers"
+            if ($ReadyDriverPack.Name -like "sp*.exe"){  #Newer HP Softpaqs are native x64 and will allow to be extracted in WinPE
+                Write-Host "Found EXE File: $($ReadyDriverPack.FullName)"
+                Show-TSActionProgress -Message "Attempting to Extract HP Driver Pack: $($ReadyDriverPack.FullName)" -Step 3 -MaxStep 5 -ErrorAction SilentlyContinue
+                try {Start-Process -FilePath $ReadyDriverPack.FullName -ArgumentList "/s /f $OSDiskDrivers\$GetMyDriverPackBaseName" -Wait}
+                catch {Write-Output "Failed to Extract"}
+                if (Test-Path "$OSDiskDrivers\$GetMyDriverPackBaseName"){
+                    Show-TSActionProgress -Message "Applying HP Driver Pack: $($OSDiskDrivers)\$($GetMyDriverPackBaseName)" -Step 4 -MaxStep 5 -ErrorAction SilentlyContinue
+                    Write-Output "Starting DISM /image:C:\ /Add-Driver /driver:$($OSDiskDrivers)\$($GetMyDriverPackBaseName) /recurse"
+                    $ArgumentList = "/image:C:\ /Add-Driver /driver:`"$OSDiskDrivers\$GetMyDriverPackBaseName`" /recurse"
+                    $null = Start-Process -FilePath 'dism.exe' -ArgumentList $ArgumentList -Wait -NoNewWindow
+                    $PPKGMethod = $false
+                }
+                else {
+                    Write-Output "Unable to Extract and Apply Driverpack in WinPE, continue to PPKG method"
+                }
+            }
+            else {
+                Write-Output "Did not find EXE file to attempt to extract"
+            }
 
-            $OSDCloudDriverPackPPKG = Join-Path (Get-Module -Name OSD -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1).ModuleBase "Provisioning\Invoke-OSDCloudDriverPack.ppkg"
+            if ($PPKGMethod -eq $true){
+                $OSDCloudDriverPackPPKG = Join-Path (Get-Module -Name OSD -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1).ModuleBase "Provisioning\Invoke-OSDCloudDriverPack.ppkg"
         
-            if (Test-Path $OSDCloudDriverPackPPKG) {
-                Write-Host
-                Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Applying first boot Specialize Provisioning Package"
-                Write-Host -ForegroundColor DarkGray "dism.exe /Image=$OSDISK\ /Add-ProvisioningPackage /PackagePath:`"$OSDCloudDriverPackPPKG`""
-                $ArgumentList = "/Image=$OSDISK\ /Add-ProvisioningPackage /PackagePath:`"$OSDCloudDriverPackPPKG`""
-                $null = Start-Process -FilePath 'dism.exe' -ArgumentList $ArgumentList -Wait -NoNewWindow
+                if (Test-Path $OSDCloudDriverPackPPKG) {
+                    Show-TSActionProgress -Message "Applying first boot Specialize Provisioning Package" -Step 4 -MaxStep 5 -ErrorAction SilentlyContinue
+                    Write-Host
+                    Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Applying first boot Specialize Provisioning Package"
+                    Write-Host -ForegroundColor DarkGray "dism.exe /Image=$OSDISK\ /Add-ProvisioningPackage /PackagePath:`"$OSDCloudDriverPackPPKG`""
+                    $ArgumentList = "/Image=$OSDISK\ /Add-ProvisioningPackage /PackagePath:`"$OSDCloudDriverPackPPKG`""
+                    $null = Start-Process -FilePath 'dism.exe' -ArgumentList $ArgumentList -Wait -NoNewWindow
+                }
             }
         }
         else {
