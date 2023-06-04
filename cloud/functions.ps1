88c6b263b3c3e08e00842d7be33eea1538e9f528
David Segura <david@segura.org>
2023-06-04 00:12:15 -0500

    Update functions.ps1

--- cloud/functions.ps1
+++ cloud/functions.ps1
@@ -88,19 +88,21 @@ function osdcloud-SetExecutionPolicy {
 function osdcloud-WinpeSetEnvironmentVariables {
     [CmdletBinding()]
     param ()
-    if (Get-Item env:LocalAppData -ErrorAction Ignore) {
-        Write-Verbose 'System Environment Variable LocalAppData is already present in this PowerShell session'
-    }
-    else {
-        Write-Host -ForegroundColor DarkGray 'Set LocalAppData in System Environment'
-        Write-Verbose 'WinPE does not have the LocalAppData System Environment Variable'
-        Write-Verbose 'This can be enabled for this Power Session, but it will not persist'
-        Write-Verbose 'Set System Environment Variable LocalAppData for this PowerShell session'
-        #[System.Environment]::SetEnvironmentVariable('LocalAppData',"$env:UserProfile\AppData\Local")
-        [System.Environment]::SetEnvironmentVariable('APPDATA',"$Env:UserProfile\AppData\Roaming",[System.EnvironmentVariableTarget]::Process)
-        [System.Environment]::SetEnvironmentVariable('HOMEDRIVE',"$Env:SystemDrive",[System.EnvironmentVariableTarget]::Process)
-        [System.Environment]::SetEnvironmentVariable('HOMEPATH',"$Env:UserProfile",[System.EnvironmentVariableTarget]::Process)
-        [System.Environment]::SetEnvironmentVariable('LOCALAPPDATA',"$Env:UserProfile\AppData\Local",[System.EnvironmentVariableTarget]::Process)
+    if ($WindowsPhase -eq 'WinPE') {
+        if (Get-Item env:LocalAppData -ErrorAction Ignore) {
+            Write-Host -ForegroundColor Green "[+] Set LocalAppData in System Environment"
+        }
+        else {
+            Write-Host -ForegroundColor Green "[+] Set LocalAppData in System Environment"
+            Write-Verbose 'WinPE does not have the LocalAppData System Environment Variable'
+            Write-Verbose 'This can be enabled for this Power Session, but it will not persist'
+            Write-Verbose 'Set System Environment Variable LocalAppData for this PowerShell session'
+            #[System.Environment]::SetEnvironmentVariable('LocalAppData',"$env:UserProfile\AppData\Local")
+            [System.Environment]::SetEnvironmentVariable('APPDATA',"$Env:UserProfile\AppData\Roaming",[System.EnvironmentVariableTarget]::Process)
+            [System.Environment]::SetEnvironmentVariable('HOMEDRIVE',"$Env:SystemDrive",[System.EnvironmentVariableTarget]::Process)
+            [System.Environment]::SetEnvironmentVariable('HOMEPATH',"$Env:UserProfile",[System.EnvironmentVariableTarget]::Process)
+            [System.Environment]::SetEnvironmentVariable('LOCALAPPDATA',"$Env:UserProfile\AppData\Local",[System.EnvironmentVariableTarget]::Process)
+        }
     }
 }
 function osdcloud-SetPowerShellProfile {
