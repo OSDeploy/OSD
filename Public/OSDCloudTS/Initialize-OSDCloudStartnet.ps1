36536d9b5faffe1796120e8817e13a0da5a42824
David Segura <david@segura.org>
2023-08-05 17:42:53 -0500

    Update Initialize-OSDCloudStartnet.ps1

--- Public/OSDCloudTS/Initialize-OSDCloudStartnet.ps1
+++ Public/OSDCloudTS/Initialize-OSDCloudStartnet.ps1
@@ -27,7 +27,8 @@ function Initialize-OSDCloudStartnet {
     )
     # Make sure we are in WinPE
     if ($env:SystemDrive -eq 'X:') {
-
+        $StartTime = Get-Date
+        Write-Host -ForegroundColor Gray "00:00 Initialize-OSDCloudStartnet.ps1"
         # Create Logs Path
         $LogsPath = "$env:SystemDrive\OSDCloud\Logs"
         if (-NOT (Test-Path -Path $LogsPath)) {
@@ -44,10 +45,11 @@ function Initialize-OSDCloudStartnet {
             Get-ChildItem "$($_.Root)OSDCloud\Config\Scripts\StartNet\" -Include "*.ps1" -File -Recurse -Force -ErrorAction Ignore
         }
         if ($Global:ScriptStartNet) {
-            Write-Host -ForegroundColor Cyan 'OSDCloud Config StartNet Scripts'
+            $TimeSpan = New-TimeSpan -Start $StartTime -End (Get-Date)
+            Write-Host -ForegroundColor Gray "$($TimeSpan.ToString("mm':'ss")) Intialize Startnet Scripts"
             $Global:ScriptStartNet = $Global:ScriptStartNet | Sort-Object -Property FullName
             foreach ($Item in $Global:ScriptStartNet) {
-                Write-Host -ForegroundColor Cyan "$($Item.FullName)"
+                Write-Host -ForegroundColor DarkGray "$($Item.FullName)"
                 & "$($Item.FullName)"
             }
         }
@@ -59,14 +61,15 @@ function Initialize-OSDCloudStartnet {
         }
 
         if ($Global:SplashScreen) {
-            Write-Host -ForegroundColor Cyan 'OSDCloud Config StartNet Scripts'
+            $TimeSpan = New-TimeSpan -Start $StartTime -End (Get-Date)
+            Write-Host -ForegroundColor Gray "$($TimeSpan.ToString("mm':'ss")) Intialize Splash Screen"
             $Global:SplashScreen = $Global:SplashScreen | Sort-Object -Property FullName
             foreach ($Item in $Global:SplashScreen) {
-                Write-Host -ForegroundColor Cyan "Found: $($Item.FullName)"
+                Write-Host -ForegroundColor DarkGray "Found: $($Item.FullName)"
             }
             if ($Global:SplashScreen.count -gt 1) {
                 $SplashJson = $Global:SplashScreen | Select-Object -Last 1
-                Write-Host -ForegroundColor Cyan "Using $($SplashJson.FullName)"
+                Write-Host -ForegroundColor DarkGray "Using $($SplashJson.FullName)"
             }
             if (Test-Path -Path "C:\OSDCloud\Logs") {
                 Remove-Item -Path "C:\OSDCloud\Logs" -Recurse -Force
@@ -76,18 +79,19 @@ function Initialize-OSDCloudStartnet {
                 Import-Module OSD -Force -ErrorAction Ignore -WarningAction Ignore
             }
             if ($Global:ModuleBase = (Get-Module -Name OSD).ModuleBase) {
-                # Write-Host -ForegroundColor Cyan "Starting $Global:ModuleBase\Resources\SplashScreen\Show-Background.ps1"
+                # Write-Host -ForegroundColor DarkGray "Starting $Global:ModuleBase\Resources\SplashScreen\Show-Background.ps1"
                 & $Global:ModuleBase\Resources\SplashScreen\Show-Background.ps1
             }
         }
 
-        # Initialize Hardware Devices
-        Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Intialize Hardware Devices"
+        $TimeSpan = New-TimeSpan -Start $StartTime -End (Get-Date)
+        Write-Host -ForegroundColor Gray "$($TimeSpan.ToString("mm':'ss")) Intialize Hardware Devices"
         Start-Sleep -Seconds 10
 
         # Initialize Wireless Network
         if (Test-Path "$env:SystemRoot\System32\dmcmnutils.dll") {
-            Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Intialize Wireless Network"
+            $TimeSpan = New-TimeSpan -Start $StartTime -End (Get-Date)
+            Write-Host -ForegroundColor Gray "$($TimeSpan.ToString("mm':'ss")) Intialize Wireless Networking"
             if ($WirelessConnect) {
                 Start-Process PowerShell -ArgumentList 'Start-WinREWiFi -WirelessConnect' -Wait
             }
@@ -97,11 +101,13 @@ function Initialize-OSDCloudStartnet {
         }
 
         # Initialize Network Connections
-        Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Intialize Network Connections"
+        $TimeSpan = New-TimeSpan -Start $StartTime -End (Get-Date)
+        Write-Host -ForegroundColor Gray "$($TimeSpan.ToString("mm':'ss")) Intialize Network Connections"
         Start-Sleep -Seconds 10
 
         # Check if the OSD Module in the PowerShell Gallery is newer than the installed version
-        Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Verify OSD PowerShell Module"
+        $TimeSpan = New-TimeSpan -Start $StartTime -End (Get-Date)
+        Write-Host -ForegroundColor Gray "$($TimeSpan.ToString("mm':'ss")) Updating OSD PowerShell Module"
         $PSModuleName = 'OSD'
         $InstalledModule = Get-Module -Name $PSModuleName -ListAvailable -ErrorAction Ignore | Sort-Object Version -Descending | Select-Object -First 1
         $GalleryPSModule = Find-Module -Name $PSModuleName -ErrorAction Ignore -WarningAction Ignore
@@ -109,35 +115,38 @@ function Initialize-OSDCloudStartnet {
         # Install the OSD module if it is not installed or if the version is older than the gallery version
         if ($GalleryPSModule) {
             if (($GalleryPSModule.Version -as [version]) -gt ($InstalledModule.Version -as [version])) {
-                Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) $PSModuleName $($GalleryPSModule.Version) [AllUsers]"
+                Write-Host -ForegroundColor DarkGray "$PSModuleName $($GalleryPSModule.Version) [AllUsers]"
                 Install-Module $PSModuleName -Scope AllUsers -Force -SkipPublisherCheck
                 Import-Module $PSModuleName -Force
             }
         }
 
-        # Generate CIM Logs
-        Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Exporting CIM logs to $LogsPath"
+        # Export Logs
+        $TimeSpan = New-TimeSpan -Start $StartTime -End (Get-Date)
+        Write-Host -ForegroundColor Gray "$($TimeSpan.ToString("mm':'ss")) Exporting Hardware logs to $LogsPath"
         
         $WIN32OPERATINGSYSTEM = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -Property *
         $WIN32OPERATINGSYSTEM | Out-File $LogsPath\Win32_OperatingSystem.txt -Width 4096 -Force
-        Write-Host -ForegroundColor Gray "WinPE" $WIN32OPERATINGSYSTEM.Version
-
         $WIN32COMPUTERSYSTEM = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property *
         $WIN32COMPUTERSYSTEM | Out-File $LogsPath\Win32_ComputerSystem.txt -Width 4096 -Force
-        Write-Host -ForegroundColor Gray "Manufacturer:" $WIN32COMPUTERSYSTEM.Manufacturer
-        Write-Host -ForegroundColor Gray "Model:" $WIN32COMPUTERSYSTEM.Model
-        Write-Host -ForegroundColor Gray "Serial Number" $WIN32BIOS.Description
-
         $WIN32BIOS = Get-WmiObject -Class Win32_BIOS | Select-Object -Property *
         $WIN32BIOS | Out-File $LogsPath\Win32_BIOS.txt -Width 4096 -Force
-        Write-Host -ForegroundColor Gray "BIOS:" $WIN32BIOS.SMBIOSBIOSVersion
-
         $WIN32PROCESSOR = Get-WmiObject -Class Win32_Processor | Select-Object -Property *
         $WIN32PROCESSOR | Out-File $LogsPath\Win32_Processor.txt -Width 4096 -Force
-        Write-Host -ForegroundColor Gray $WIN32PROCESSOR.Name "|" $WIN32PROCESSOR.NumberOfCores "Cores |" $WIN32PROCESSOR.NumberOfLogicalProcessors "Logical Processors"
+
+        Write-Host -ForegroundColor Gray "WinPE" $WIN32OPERATINGSYSTEM.BuildNumber $WIN32OPERATINGSYSTEM.OSArchitecture '|' $WIN32OPERATINGSYSTEM.Version
+        Write-Host -ForegroundColor Gray $WIN32COMPUTERSYSTEM.Manufacturer '|' $WIN32COMPUTERSYSTEM.Model
+        Write-Host -ForegroundColor Gray "Product" $(Get-MyComputerProduct) "| Serial Number" $WIN32BIOS.SerialNumber
+        Write-Host -ForegroundColor Gray "BIOS" $WIN32BIOS.SMBIOSBIOSVersion
+        Write-Host -ForegroundColor Gray $WIN32PROCESSOR.Name "|" $WIN32PROCESSOR.NumberOfLogicalProcessors "Logical Processors"
+        Write-Host -ForegroundColor Gray $WIN32OPERATINGSYSTEM.TotalVisibleMemorySize "MB Memory"
+
+
+$memory = [math]::Round((Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB)
+Write-Host -ForegroundColor Gray "System Memory: $memory GB"
 
         # Start PowerShell minimized
-        Write-Host -ForegroundColor Cyan "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Start PowerShell Session (minimized)"
+        Write-Host -ForegroundColor DarkGray "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Start PowerShell Session (minimized)"
         Start-Process PowerShell -WindowStyle Minimized
 
         # Force import the OSD module
