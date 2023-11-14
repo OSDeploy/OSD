1fee9a7b4e441ab7a490fc34a44b2b35d4003c4d
gwblok <garywblok@gmail.com>
2023-11-14 15:30:01 -0600

    MCT Data for ESD Files

--- build/Build-OSDCloudOperatingSystemsv3.ps1
+++ build/Build-OSDCloudOperatingSystemsv3.ps1
@@ -15,11 +15,6 @@ $WindowsTable = @(
 @{ Version = 'Win1121H2';LocalCab = "Win1122H2.Cab"; URL = "https://download.microsoft.com/download/b/1/9/b19bd7fd-78c4-4f88-8c40-3e52aee143c2/products_win11_20230510.cab.cab"}
 )
 
-$Win1022H2CabURL = "https://download.microsoft.com/download/3/c/9/3c959fca-d288-46aa-b578-2a6c6c33137a/products_win10_20230510.cab.cab"
-$Win1121H2CabURL = "https://download.microsoft.com/download/1/b/4/1b4e06e2-767a-4c9a-9899-230fe94ba530/products_Win11_20211115.cab"
-$Win1122H2CabURL = "https://download.microsoft.com/download/b/1/9/b19bd7fd-78c4-4f88-8c40-3e52aee143c2/products_win11_20230510.cab.cab"
-
-
 
 #region functions borrowed from HPCMSL
 function Invoke-HPPrivateExpandCAB {
