7e351b779c8d850dbf7f8fc2f140743e97af8214
gwblok <garywblok@gmail.com>
2023-12-11 16:33:14 -0600

    Update Get-PowerSettingTurnMonitorOffAfter.ps1

--- Public/OSDCloudTS/Get-PowerSettingTurnMonitorOffAfter.ps1
+++ Public/OSDCloudTS/Get-PowerSettingTurnMonitorOffAfter.ps1
@@ -1,4 +1,4 @@
-﻿function Get-PowerSettingTurnMonitorOffAfter{
+﻿Function Get-PowerSettingTurnMonitorOffAfter{
     # Get active plan
     # Get-CimInstance won't work due to Get-CimInstance -Namespace root\cimv2\power -ClassName Win32_PowerPlan doesn't have the "Activate" trigger as Get-WmiObject does
     $CurrentPlan = Get-WmiObject -Namespace root\cimv2\power -ClassName Win32_PowerPlan | Where-Object -FilterScript {$_.IsActive}
