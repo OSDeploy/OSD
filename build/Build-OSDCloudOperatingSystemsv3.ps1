27f9d23c85bcad1b04ec677e30df800a41c3bebe
gwblok <garywblok@gmail.com>
2023-12-23 09:10:02 -0600

    Update Build-OSDCloudOperatingSystemsv3.ps1

--- build/Build-OSDCloudOperatingSystemsv3.ps1
+++ build/Build-OSDCloudOperatingSystemsv3.ps1
@@ -1,7 +1,14 @@
-#Get ESD files from the Microsoft Creation Tool's list
-#This file needs to be updated for each release of Windows
-#This will create a table of all of the ESD files from MS, then create a "database" for OSDCloud
-#Gary Blok's attempt to help David
+<#
+Get ESD files from the Microsoft Creation Tool's list
+This file needs to be updated for each release of Windows
+This will create a table of all of the ESD files from MS, then create a "database" for OSDCloud
+Gary Blok's attempt to help David
+
+Changes
+23.12.23 - Updated Win11 23H2 CAB URL for newer ESD files
+
+#>
+
 # Import OSD Module
 Import-Module OSD -Force -ErrorAction Stop
 
@@ -14,7 +21,7 @@ $WindowsTable = @(
 @{ Version = 'Win1022H2';LocalCab = "Win1022H2.Cab"; URL = "https://download.microsoft.com/download/3/c/9/3c959fca-d288-46aa-b578-2a6c6c33137a/products_win10_20230510.cab.cab"}
 @{ Version = 'Win1121H2';LocalCab = "Win1121H2.Cab"; URL = "https://download.microsoft.com/download/1/b/4/1b4e06e2-767a-4c9a-9899-230fe94ba530/products_Win11_20211115.cab"}
 @{ Version = 'Win1122H2';LocalCab = "Win1122H2.Cab"; URL = "https://download.microsoft.com/download/b/1/9/b19bd7fd-78c4-4f88-8c40-3e52aee143c2/products_win11_20230510.cab.cab"}
-@{ Version = 'Win1123H2';LocalCab = "Win1123H2.Cab"; URL = "https://download.microsoft.com/download/e/8/6/e86b4c6f-4ae8-40df-b983-3de63ea9502d/products_win11_202311109.cab"}
+@{ Version = 'Win1123H2';LocalCab = "Win1123H2.Cab"; URL = "https://download.microsoft.com/download/6/2/b/62b47bc5-1b28-4bfa-9422-e7a098d326d4/products_win11_20231208.cab"}
 )
 
 
