b2c53ff9b61ec32f951bf96a44455e43bed0e166
David Segura <david@segura.org>
2024-02-22 10:08:56 -0600

    24.2.22.1

--- cloud/subdomains/tpm.osdcloud.com.ps1
+++ cloud/subdomains/tpm.osdcloud.com.ps1
@@ -71,6 +71,93 @@ Write-Host -ForegroundColor Green "[+] Transport Layer Security (TLS) 1.2"
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 #endregion
 
+
+#region TPM and Autopilot
+function Test-TpmCimInstance {
+    <#
+IsActivated_InitialValue    : True
+IsEnabled_InitialValue      : True
+IsOwned_InitialValue        : True
+ManufacturerId              : 1314145024
+ManufacturerIdTxt           : NTC
+ManufacturerVersion         : 7.2.3.1
+ManufacturerVersionFull20   : 7.2.3.1
+ManufacturerVersionInfo     : NPCT75x 
+PhysicalPresenceVersionInfo : 1.3
+SpecVersion                 : 2.0, 0, 1.59
+PSComputerName              : 
+#>
+    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM'" -ForegroundColor Cyan
+    $Win32Tpm = Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'Win32_TPM' -ErrorAction SilentlyContinue
+    if ($Win32Tpm) {
+        $Win32Tpm
+        if ($Win32Tpm.IsEnabled_InitialValue -ne $true) {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) IsEnabled_InitialValue should be True for Autopilot to work properly"
+        }
+
+        if ($Win32Tpm.IsActivated_InitialValue -ne $true) {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) IsActivated_InitialValue should be True"
+        }
+    
+        if ($Win32Tpm.IsOwned_InitialValue -ne $true) {
+            Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) IsOwned_InitialValue should be True"
+        }
+        if (!(Get-Tpm | Select-Object tpmowned).TpmOwned -eq $true) {
+            Write-Warning 'Reason: TpmOwned is not owned!)'
+        }
+
+        $IsReady = $Win32Tpm | Invoke-CimMethod -MethodName 'IsReadyInformation'
+        $IsReadyInformation = $IsReady.Information
+        if ($IsReadyInformation -eq '0') {
+            Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) IsReadyInformation $IsReadyInformation TPM is ready for attestation"
+        }
+        else {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) IsReadyInformation $IsReadyInformation TPM is not ready for attestation"
+        }
+        if ($IsReadyInformation -eq '16777216') {
+            Write-Warning 'The TPM has a Health Attestation related vulnerability'
+        }
+    }
+    else {
+        Write-Warning 'FAIL: Unable to get TPM information'
+    }
+}
+function Test-TpmRegistryEkCert {
+    $RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Services\Tpm\WMI\Endorsement\EKCertStore\Certificates\*'
+    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Registry Test: Windows EKCert" -ForegroundColor Cyan
+    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) $RegistryPath" -ForegroundColor DarkGray
+
+    if (Test-Path -Path $RegistryPath) {
+        $EKCert = Get-ItemProperty -Path $RegistryPath
+        $EKCert | Format-List
+    }
+    else {
+        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) EKCert was not found in the Registry"
+    }
+}
+function Test-TpmRegistryWBCL {
+    $RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\IntegrityServices\*'
+    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Registry Test: Windows Boot Configuration Log" -ForegroundColor Cyan
+    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) $RegistryPath -Value WBCL" -ForegroundColor DarkGray
+
+    if (Test-Path -Path $RegistryPath) {
+        $WBCL = (Get-ItemProperty -Path $RegistryPath).WBCL
+        if ($null -ne $WBCL) {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) WBCL was not found in the Registry"
+            Write-Warning 'Measured boot logs are missing.  Reboot may be required.'
+        }
+    }
+    else {
+        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) WBCL was not found in the Registry"
+        Write-Warning 'Measured boot logs are missing.  Reboot may be required.'
+    }
+}
+#endregion
+
+
+
+
+
 #region WinPE
 if ($WindowsPhase -eq 'WinPE') {
 
