fdec13a7caa10594a0c91a24cbe6fb7f17f7b42b
David Segura <david@segura.org>
2024-02-22 11:00:04 -0600

    Update tpm.osdcloud.com.ps1

--- cloud/subdomains/tpm.osdcloud.com.ps1
+++ cloud/subdomains/tpm.osdcloud.com.ps1
@@ -133,24 +133,28 @@ function Test-TpmRegistryEkCert {
         $EKCert | Format-List
     }
     else {
-        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) EKCert was not found in the Registry"
+        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) EKCert key was not found in the Registry"
     }
 }
 function Test-TpmRegistryWBCL {
     $RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\IntegrityServices\*'
     Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Test Windows Boot Configuration Log in the Registry" -ForegroundColor Cyan
-    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) $RegistryPath -Value WBCL" -ForegroundColor DarkGray
+    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) $RegistryPath" -ForegroundColor DarkGray
 
     if (Test-Path -Path $RegistryPath) {
-        $WBCL = (Get-ItemProperty -Path $RegistryPath).WBCL
+        $WBCL = Get-ItemProperty -Path $RegistryPath
+        $WBCL | Format-List
+
+
+        <#$WBCL = (Get-ItemProperty -Path $RegistryPath).WBCL
         if ($null -ne $WBCL) {
             Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) WBCL was not found in the Registry"
             Write-Warning 'Measured boot logs are missing.  Reboot may be required.'
-        }
+        }#>
     }
     else {
-        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) WBCL was not found in the Registry"
-        Write-Warning 'Measured boot logs are missing.  Reboot may be required.'
+        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) WBCL key was not found in the Registry"
+        Write-Warning 'Measured boot logs are missing.  A Reboot may be required.'
     }
 }
 
