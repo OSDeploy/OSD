c58e6d113a72d79dc4ad526aedf1d029b0e260ea
David Segura <david@segura.org>
2024-02-22 21:57:55 -0600

    Update tpm.osdcloud.com.ps1

--- cloud/subdomains/tpm.osdcloud.com.ps1
+++ cloud/subdomains/tpm.osdcloud.com.ps1
@@ -85,6 +85,114 @@ SpecVersion                 : 2.0, 0, 1.59
 PSComputerName              : 
 #>
 
+#region TpmCloud Configuration
+$Global:TpmCloud = $null
+$Global:TpmCloud = [ordered]@{
+    IsTpmPresent                    = $null
+    TpmNamespace                    = 'root/cimv2/Security/MicrosoftTpm'
+    TpmClass                        = 'Win32_Tpm'
+
+
+
+    IsAutopilotReady                = $true
+    IsTpmReady                      = $true
+    IsTpmV2                         = $null
+    GetTpmClass                     = $null
+    GetTpmIsReadyInformation        = $null
+    MicrosoftConnectionUri          = 'http://www.msftconnecttest.com/connecttest.txt'
+    TestMicrosoftConnection         = $true
+    ResultMicrosoftConnection       = $null
+    EKCertificatesRegPath           = 'HKLM:\SYSTEM\CurrentControlSet\Services\Tpm\WMI\Endorsement\EKCertStore\Certificates\*'
+    EKCertificatesRegData           = $null
+    MeasuredBootRegPath             = 'HKLM:\SYSTEM\CurrentControlSet\Control\IntegrityServices'
+    MeasuredBootRegProperty         = 'WBCL'
+    MeasuredBootRegData             = $null
+    TpmToolGetDeviceInformation     = $null
+    GetTpmEndorsementKeyInfo        = $null
+    PSTrustedPlatformModule         = (Get-Module -Name TrustedPlatformModule)
+}
+#endregion
+
+function Test-GetTpmClass {
+    Write-Host -ForegroundColor DarkGray '========================================================================='
+    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Get TPM CimInstance" -ForegroundColor Cyan
+    Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Get-CimInstance -Namespace $($Global:TpmCloud.TpmNamespace) -ClassName $($Global:TpmCloud.TpmClass)" -ForegroundColor DarkGray
+
+    $Global:TpmCloud.GetTpmClass = Get-CimInstance -Namespace $($Global:TpmCloud.TpmNamespace) -ClassName $($Global:TpmCloud.TpmClass) -ErrorAction SilentlyContinue
+
+    if ($Global:TpmCloud.GetTpmClass) {
+        $Global:TpmCloud.IsTpmPresent = [bool]$true
+        $Global:TpmCloud.GetTpmClass
+
+        if ($Global:TpmCloud.GetTpmClass.IsEnabled_InitialValue -ne $true) {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM is not enabled."
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Autopilot will fail."
+            $Global:TpmCloud.GetTpmClass.IsTpmReady = [bool]$false
+            $Global:TpmCloud.GetTpmClass.IsAutopilotReady = [bool]$false
+        }
+        if ($Global:TpmCloud.GetTpmClass.IsActivated_InitialValue -ne $true) {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM is not yet activated."
+        }
+        if ($Global:TpmCloud.GetTpmClass.IsOwned_InitialValue -ne $true) {
+            Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM is not owned." -ForegroundColor DarkGray
+            Write-Host "Windows automatically initializes and takes ownership of the TPM. There's no need for you to initialize the TPM and create an owner password." -ForegroundColor DarkGray
+            Write-Host 'https://learn.microsoft.com/en-us/windows/security/hardware-security/tpm/initialize-and-configure-ownership-of-the-tpm' -ForegroundColor DarkGray
+        }
+        if ($Global:TpmCloud.GetTpmClass.SpecVersion -like '*2.0*') {
+            Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM version is 2.0." -ForegroundColor DarkGray
+            Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Attestation requires TPM 2.0." -ForegroundColor DarkGray
+            $Global:TpmCloud.IsTpmV2 = [bool]$true
+        }
+        else {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM version is not 2.0."
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Attestation requires TPM 2.0."
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Autopilot will fail."
+            $Global:TpmCloud.IsTpmV2 = [bool]$false
+            $Global:TpmCloud.IsTpmReady = [bool]$false
+            $Global:TpmCloud.IsAutopilotReady = [bool]$false
+        }
+        Write-Host -ForegroundColor DarkGray '========================================================================='
+        Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Get TPM CimMethod IsReadyInformation" -ForegroundColor Cyan
+        $Global:TpmCloud.GetTpmIsReadyInformation = Get-CimInstance -Namespace $($Global:TpmCloud.TpmNamespace) -ClassName $($Global:TpmCloud.TpmClass) -ErrorAction SilentlyContinue | Invoke-CimMethod -MethodName 'IsReadyInformation'
+        $Global:TpmCloud.GetTpmIsReadyInformation
+        if ($Global:TpmCloud.GetTpmIsReadyInformation.Information -eq '0') {
+            Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM is ready for attestation." -ForegroundColor DarkGray
+        }
+        else {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM is not ready for attestation."
+            $Global:TpmCloud.IsTpmReady = [bool]$false
+            $Global:TpmCloud.IsAutopilotReady = [bool]$false
+        }
+        if ($Global:TpmCloud.GetTpmIsReadyInformation.Information -eq '16777216') {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM has a Health Attestation related vulnerability."
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Autopilot will fail."
+            $Global:TpmCloud.IsTpmReady = [bool]$false
+            $Global:TpmCloud.IsAutopilotReady = [bool]$false
+        }
+        if ($Global:TpmCloud.GetTpmIsReadyInformation.Information -eq '262144') {
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) TPM EK Certificate is missing or invalid."
+            Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Autopilot will fail."
+            $Global:TpmCloud.IsTpmReady = [bool]$false
+            $Global:TpmCloud.IsAutopilotReady = [bool]$false
+        }
+    }
+    else {
+        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Unable to get TPM information."
+        Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Autopilot will fail."
+        $Global:TpmCloud.IsTpmPresent = [bool]$false
+        $Global:TpmCloud.IsTpmReady = [bool]$false
+        $Global:TpmCloud.IsAutopilotReady = [bool]$false
+    }
+}
+
+
+
+
+
+$Global:TpmCloud.TpmToolGetDeviceInformation = tpmtool.exe GetDeviceInformation
+
+
+
 #region TpmCloud Tests
 function Test-MicrosoftConnection {
     try {
@@ -100,36 +208,8 @@ function Test-MicrosoftConnection {
     }
 }
 #endregion
-
-
-
-#region TpmCloud Configuration
-$Global:TpmCloud = $null
-$Global:TpmCloud = [ordered]@{
-    IsAutopilotReady                = $true
-    IsTpmReady                      = $true
-    IsTpmV2                         = $null
-    GetTpmClass                     = $null
-    GetTpmIsReadyInformation        = $null
-    TpmNamespace                    = 'root/cimv2/Security/MicrosoftTpm'
-    TpmClass                        = 'Win32_Tpm'
-    MicrosoftConnectionUri          = 'http://www.msftconnecttest.com/connecttest.txt'
-    TestMicrosoftConnection         = $true
-    ResultMicrosoftConnection       = $null
-
-    EKCertificatesRegPath           = 'HKLM:\SYSTEM\CurrentControlSet\Services\Tpm\WMI\Endorsement\EKCertStore\Certificates\*'
-    EKCertificatesRegData           = $null
-
-    MeasuredBootRegPath         = 'HKLM:\SYSTEM\CurrentControlSet\Control\IntegrityServices'
-    MeasuredBootRegProperty     = 'WBCL'
-    MeasuredBootRegData         = $null
-}
-#endregion
-
-
-
 #region TPM and Autopilot
-function Test-GetTpmClass {
+function Test-GetTpmClassss {
     Write-Host -ForegroundColor DarkGray '========================================================================='
     Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Get TPM CimInstance" -ForegroundColor Cyan
     Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Get-CimInstance -Namespace $($Global:TpmCloud.TpmNamespace) -ClassName $($Global:TpmCloud.TpmClass)" -ForegroundColor DarkGray
@@ -197,6 +277,18 @@ function Test-GetTpmClass {
         $Global:TpmCloud.IsAutopilotReady = [bool]$false
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 function Test-EKCertificatesRegPath {
     Write-Host -ForegroundColor DarkGray '========================================================================='
     $RegistryPath = $Global:TpmCloud.EKCertificatesRegPath
@@ -235,7 +327,6 @@ function Test-TpmRegistryWBCL {
         Write-Warning "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Measured boot logs are missing.  A Reboot may be required"
     }
 }
-
 function Test-AutopilotUrl {
     Write-Host -ForegroundColor DarkGray '========================================================================='
     Write-Host "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) Test Autopilot URLs" -ForegroundColor Cyan
@@ -438,14 +529,17 @@ if ($WindowsPhase -eq 'OOBE') {
 if ($WindowsPhase -eq 'Windows') {
     osdcloud-SetExecutionPolicy
     #osdcloud-SetPowerShellProfile
-    Test-WindowsTimeService
-    Test-AutopilotUrl
-    Test-AutopilotWindowsLicense
-    Test-AzuretUrl
-    Test-TpmUrl
-    Test-GetTpmClass 
-    Test-RegKeyEKCert
-    Test-TpmRegistryWBCL
+
+    Test-GetTpmClass
+    if ($Global:TpmCloud.IsTpmPresent) {
+        Test-WindowsTimeService
+        Test-AutopilotUrl
+        Test-AutopilotWindowsLicense
+        Test-AzuretUrl
+        Test-TpmUrl
+        Test-RegKeyEKCert
+        Test-TpmRegistryWBCL
+    }
     Write-Host -ForegroundColor Green "[+] tpm.osdcloud.com Complete"
     $null = Stop-Transcript -ErrorAction Ignore
 }
