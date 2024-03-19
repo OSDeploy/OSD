ce36a6a6dd9e584563e5ec7115aa660d1b449b14
gwblok <garywblok@gmail.com>
2024-03-19 17:10:47 -0500

    24.3.19.4 - Issue 126

--- Public/OSDCloudSetup/OSDCloudWinPE.ps1
+++ Public/OSDCloudSetup/OSDCloudWinPE.ps1
@@ -215,7 +215,9 @@
     #region CloudDriver
     if ($CloudDriver) {
         foreach ($Driver in $CloudDriver) {
+            Write-Verbose "Downloading $Driver Driver Pack"
             $AddWindowsDriverPath = Save-WinPECloudDriver -CloudDriver $Driver -Path (Join-Path $env:TEMP (Get-Random))
+            Write-Verbose "Adding $AddWindowsDriverPath to WinPE Drivers"
             Add-WindowsDriver -Path "$MountPath" -Driver "$AddWindowsDriverPath" -Recurse -ForceUnsigned -Verbose
         }
         $null = Save-WindowsImage -Path $MountPath
