a86ddb445cc6297c1dccd9bbea94460d44e3517a
gwblok <garywblok@gmail.com>
2024-05-21 16:02:19 -0500

    24.5.21

---
+++ Public/OSDCloudTS/Install-LenovoApps.ps1
@@ -0,0 +1,29 @@
+function Install-LenovoSystemUpdater {
+    # Define the URL and temporary file path
+    $url = "https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.08.02.25.exe"
+    $tempFilePath = "C:\Windows\Temp\system_update.exe"
+
+    # Create a new BITS transfer job
+    $bitsJob = Start-BitsTransfer -Source $url -Destination $tempFilePath
+
+    # Wait for the BITS transfer job to complete
+    while ($bitsJob.JobState -eq "Transferring") {
+        Start-Sleep -Seconds 1
+    }
+
+    # Check if the transfer was successful
+    if ($bitsJob.JobState -eq "Transferred") {
+        # Start the installation process
+        Write-Host -ForegroundColor Green "Installation file downloaded successfully. Starting installation..."
+        $ArgumentList = "/VERYSILENT /NORESTART"
+        $InstallProcess = Start-Process -FilePath $tempFilePath -ArgumentList $ArgumentList -Wait -PassThru
+        if ($InstallProcess.ExitCode -eq 0) {
+            Write-Host -ForegroundColor Green "Installation completed successfully."
+        } else {
+            Write-Host -ForegroundColor Red "Installation failed with exit code $($InstallProcess.ExitCode)."
+        }
+    } else {
+        Write-Host "Failed to download the file."
+    }
+}
+
