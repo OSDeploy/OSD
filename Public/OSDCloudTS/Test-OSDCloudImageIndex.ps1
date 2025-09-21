6dc70f4ef9ff9a674ed6432afed165100e1bf237
ricoroodenburg <79546116+ricoroodenburg@users.noreply.github.com>
2025-09-21 13:27:46 +0200

    Create Test-OSDCloudImageIndex.ps1

---
+++ Public/OSDCloudTS/Test-OSDCloudImageIndex.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+Test if Image Index exists
+
+.DESCRIPTION
+Test if Image Index exists
+
+.LINK
+https://github.com/OSDeploy/OSD/tree/master/Docs
+#>
+
+function Test-OSDCloudImageIndex {
+    [CmdletBinding()]
+    param (
+        [string]$ImagePath,
+        [int]$Index
+    )
+
+    $Image = Get-WindowsImage -ImagePath $ImagePath -Index $Index -ErrorAction Ignore
+
+    If ($Image){
+    
+         Return $Image.ImageIndex
+
+    }
+
+}
