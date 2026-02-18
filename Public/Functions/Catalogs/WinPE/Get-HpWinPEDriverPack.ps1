9117729a3ba74df3423f74cb9087a05ce656e9ba
David Segura <david@segura.org>
2026-02-18 13:24:03 -0600

    fix(WinPEDriverPack): update HP WinPE 10 and 11 driver pack URLs

--- Public/Functions/Catalogs/WinPE/Get-HpWinPEDriverPack.ps1
+++ Public/Functions/Catalogs/WinPE/Get-HpWinPEDriverPack.ps1
@@ -13,6 +13,15 @@ function Get-HpWinPEDriverPack {
     [CmdletBinding()]
     param ()
 
+    # WinPE Driver Packs
+    # https://ftp.hp.com/pub/caps-softpaq/cmit/HP_WinPE_DriverPack.html
+
+    # WinPE 10
+    # http://ftp.ext.hp.com/pub/caps-softpaq/cmit/softpaq/WinPE10.html
+
+    # WinPE 11
+    # http://ftp.ext.hp.com/pub/caps-softpaq/cmit/softpaq/WinPE11.html
+
     $LastKnownGood = $Global:OSDModuleResource.WinPEDriverPack.HP.LastKnownGood
     $DriverPackInfoUrl = $Global:OSDModuleResource.WinPEDriverPack.HP.Info
 
