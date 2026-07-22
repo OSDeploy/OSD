38549d2163edf2b74a97a1d6ad4cbd83389e7d41
David Segura <david@segura.org>
2026-07-21 23:21:12 -0500

    feat(disk): add functions to retrieve available drive letters and FFU source/destination disks

--- Private/disk/ffu.ps1
+++ Private/disk/ffu.ps1
@@ -42,7 +42,7 @@ function Get-FFUDestinationDisks {
     $LocalResults = foreach ($Item in $GetPartition) {
         $GetVolumeProperties = $GetVolume | Where-Object {$_.DriveLetter -eq $Item.DriveLetter}
         $ObjectProperties = @{
-            
+
             DiskNumber          = $Item.DiskNumber
             DriveLetter         = $GetVolumeProperties.DriveLetter
             FileSystem          = $GetVolumeProperties.FileSystem
@@ -80,4 +80,28 @@ function Get-FFUDestinationDisks {
     $LocalResults = $LocalResults | Where-Object {$_.FileSystem -eq 'NTFS'}
     [array]$Results = [array]$LocalResults + [array]$NetworkResults
     Return [array]$Results
+}
+function Get-FFUSourceDisks {
+    <#
+    .SYNOPSIS
+    Returns eligible source disks for FFU capture.
+
+    .DESCRIPTION
+    Filters local disks to include online, non-offline, non-boot disks with at
+    least one partition and a nonzero size.
+
+    .EXAMPLE
+    Get-FFUSourceDisks
+    Returns disks that can be selected as FFU source disks.
+
+    .LINK
+    https://github.com/OSDeploy/OSD/tree/master/docs
+
+    .NOTES
+    Author: David Segura - Recast Software
+    2026-07-16 - Added initial in-function help block
+    #>
+    [CmdletBinding()]
+    param ()
+    Get-LocalDisk | Where-Object {$_.NumberOfPartitions -ge '1'} | Where-Object {$_.OperationalStatus -eq 'Online'} | Where-Object {$_.Size -gt 0} | Where-Object {$_.IsOffline -eq $false} | Where-Object {$_.IsBoot -eq $false}
 }
